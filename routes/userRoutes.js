de4941ecb335be50b97576f464700a1aef09358e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-31 12:29:27 +0600

    user routes  🤩

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userControllers");
 const authMiddleware = require("../middleware/authMiddleware");
-
+// user routes
 router.get("/", userController.getHomepage);
 router.get("/signup", userController.getSignupPage);
 router.get("/login", userController.getLoginPage);
